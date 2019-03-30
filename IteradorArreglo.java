0d7116d998be6d82eeaf7c4d353d2f7dabac68aa
IlseCS <47673144+IlseCS@users.noreply.github.com>
2019-03-29 20:09:52 -0600

    Clase iterador

---
+++ IteradorArreglo.java
@@ -0,0 +1,40 @@
+package proyecto_2_ed;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class IteradorArreglo <T> implements Iterator <T> {
+    private T[] coleccion;
+    private int total;
+    private int actual;
+    
+    //constructor
+    public IteradorArreglo (int tot, T[] arre){
+        coleccion=arre;
+        total=tot;
+        actual=0;
+    }
+
+    public boolean hasNext() {
+        return actual<total;
+    }
+
+    /*
+    da acceso al que está apuntando y se va al siguiente
+    */
+    public T next() {
+        if (hasNext()){
+            T resp;
+            resp=coleccion[actual];
+            actual++;
+            return resp;
+        }//cierre del if
+        else
+            throw new NoSuchElementException();
+    }//fin del método
+
+    public void remove() {
+        throw new UnsupportedOperationException("Operación no válida");
+    }
+    
+}//fin de clase
